d9ede66e2a579c9a1d86f7d9e4b9d7f9ef23cdb4
Benoit Niveau <be.niveau@hotmail.fr>
2019-01-03 16:14:30 +0100

    age_input

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -6,6 +6,7 @@
         <input type="email" name="email" placeholder="Email">
         <input type="password" name="password" placeholder="Mot de passe">
         <input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)">
+        <input type="number" name="age" placeholder="Age">
         <input type="submit" value="M'inscrire">
     </form>
 @endsection
