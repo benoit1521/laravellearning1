0a414db4774ed8f35e8230677b0200b6b8846ae9
Benoit Niveau <be.niveau@hotmail.fr>
2019-01-05 19:35:23 +0100

    bootstrap_on_inscritption

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -3,10 +3,11 @@
 @section('contenu')
     <form action="/inscription" method="post">
     {{ csrf_field() }}
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)">
-        <input type="number" name="age" placeholder="Age">
+
+        <input type="email" name="email" placeholder="Email" class="form-control col-3">
+        <input type="password" name="password" placeholder="Mot de passe"class="form-control col-3">
+        <input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)"class="form-control col-3">
+        <input type="number" name="age" placeholder="Age"class="form-control col-3">
         <input type="submit" value="M'inscrire" class="btn btn-primary">
     </form>
 @endsection
